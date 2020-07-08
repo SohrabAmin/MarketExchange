6492527da990365e22a09ec82c0d401a670c418f
tsantina <tina.tsan@mail.utoronto.ca>
2020-07-08 10:14:05 -0400

    added javadoc and comments to writing.java

--- phase1/src/writing.java
+++ phase1/src/writing.java
@@ -1,40 +1,37 @@
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Read and writes the UserList file in order to populate UserManager. This code is in the Week 6 Module from logging.
- */
+// NOTE: This class is based off of DemoManager from logging in Week 6 Modules on Quercus.
+
+
 public class writing {
 
-    //public static void main(String[] args) throws IOException, ClassNotFoundException {
-
-    //}
-
+    /**
+     * Read and writes the UserList file in order to populate UserManager.
+     */
     public static void demoUserRead(String s, UserManager u) throws IOException, ClassNotFoundException {
 
         String serializedUserInfo = "src/UserList3.ser";
         // creates UserReadWrite which manages the saving and loading users.
         UserReadWrite userRW = new UserReadWrite(serializedUserInfo);
-        //System.out.println("Initial state:\n" + userRW);
 
 
 //    // Deserializes contents of the SER file
         userRW.readFromFile(serializedUserInfo);
         userRW.populateUserManager(u);
 
-        // adds a new student to UserReadWrite sm's records
-        //userRW.add(new User("Tina", "123"));
-        //userRW.add(new User("yoyoo", "12322"));
-        //System.out.println("After:\n" + userRW);
-
-        // Writes the existing Student objects to file.
-        // This data is serialized and written to file as a sequence of bytes.
-        //userRW.saveToFile(serializedUserInfo);
     }
 
-    public static void demoUserSave(UserManager u) throws IOException, ClassNotFoundException {
+    /**
+     *  Saves the users from userManager into the UserList file.
+     *
+     * @param userManager
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static void demoUserSave(UserManager userManager) throws IOException, ClassNotFoundException {
         String serializedUserInfo = "src/UserList3.ser";
         UserReadWrite userRW = new UserReadWrite(serializedUserInfo);
-        userRW.saveToFile(serializedUserInfo, u);
+        userRW.saveToFile(serializedUserInfo, userManager);
     }
 }
