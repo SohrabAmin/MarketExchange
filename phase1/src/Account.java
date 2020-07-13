dd06bac0684e6c81bb5db363cb11788bb755e8f6
liuxinzh <xinzhu.liu@mail.utoronto.ca>
2020-07-13 18:38:04 -0400

    add javadocs but not finished. update methods for pendingTrade

--- phase1/src/Account.java
+++ phase1/src/Account.java
@@ -1,17 +1,23 @@
 import java.io.Serializable;
-
+/**
+ * Represent an account with name and corresponding password
+ */
 public abstract class Account implements Serializable {
      protected String name;
      protected String password;
 
      /**
       * getter for an account password
-      * @return this account password
+      * @return this account's password
       */
      public String getPassword() {
           return password;
      }
 
+     /**
+      * getter for an account name
+      * @return this account's name
+      */
      public String getName(){
           return name;
      }
--- phase1/src/User.java
+++ phase1/src/User.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * represent a User with name and password. Show user's wishlist, inventory, draft inventory( items added but not
+ * approved or declined by admin),
+ */
 public class User extends Account {
 
     private ArrayList<Item> wishlist;
@@ -14,13 +18,21 @@ public class User extends Account {
     private List<User> topTradingPartners;
     private List<TradeRequest> pendingRequests;
     private List<TradeRequest> outboundRequests;
-    private List<Meeting> pendingTrades;
+    private List<Transaction> pendingTrades;
     private boolean isPseudoFrozen;
     private List<Transaction> cancelledTransactions;
 
 
     public User() {
     }
+    /**
+     * constructs an instance of User with name and password and a list of wishlist, inventory
+     * draft inventory( items waiting for admin's approval), trade history, pending request (received but not approve
+     * declined),
+     *
+     * @param name of this user as a string
+     * @param password of this user as a string
+     */
 
     public User(String name, String password) {
         this.name = name;
@@ -38,54 +50,106 @@ public class User extends Account {
 
     }
 
+    /**
+     * getter for
+     * @return
+     */
+
     public HashMap<Item, String> getItemHistory() {
         return ItemHistory;
     }
-
+    /**
+     * getter for user's wishlist
+     * @return this user's withlist as a list
+     */
     public ArrayList<Item> getWishlist() {
         return wishlist;
     }
 
+    /**
+     * getter for user's inventory
+     * @return this user's inventory as a ArrayList
+     */
     public ArrayList<Item> getInventory() {
         return inventory;
     }
 
+    /**
+     * getter for user's draft inventory which invluding the items the user add to the inventory but waiting for admin's approval.
+     * @return this user's draft inventory as a ArrayList
+     */
     public ArrayList<Item> getDraftInventory() {
         return draftInventory;
     }
 
+    /**
+     * getter for user's sucessful Trade History
+     * @return this user's past sucessful trade as a list of transactions.
+     */
     public List<Transaction> getTradeHistory() {
         return this.tradeHistory;
     }
 
+    /**
+     * setter for user's sucessful trade history
+     * @param transaction to add a transaction to this user's trade history
+     */
     public void addTradeHistory(Transaction transaction) {
         this.tradeHistory.add(transaction);
     }
 
+    /**
+     * getter for user's pending requests that user receives
+     * @return this user's pending requests as a List.
+     */
     public List<TradeRequest> getPendingRequests() {
         return this.pendingRequests;
     }
 
+    /**
+     * getter
+     * @return
+     */
     public List<User> getTopTradingPartners() {
         return topTradingPartners;
     }
 
+    /**
+     * getter for trade requests that user send out for a trade
+     * @return trade requests sent by this user as a List.
+     */
     public List<TradeRequest> getOutboundRequests() {
         return outboundRequests;
     }
 
-    public List<Meeting> getPendingTrades() {
+    /**
+     * getter for the trades that is not completed but the meeting has been set up
+     * @return List of transactions for this user's trade that is not yet completed
+     */
+    public List<Transaction> getPendingTrades() {
         return pendingTrades;
     }
 
+    /**
+     * setter for the requests sent out by the user
+     * @param outboundRequest to add Trade requests to the list of requests sent out by this user
+     */
     public void addOutboundRequest(TradeRequest outboundRequest) {
         outboundRequests.add(outboundRequest);
     }
 
-    public void addPendingTrade(Meeting meeting) {
-        pendingTrades.add(meeting);
+    /**
+     * setter for the pending trade.
+     * @param transaction to add meeting information to the trade that is not completed.
+     */
+    public void addPendingTrade(Transaction transaction) {
+        pendingTrades.add(transaction);
     }
 
+    /**
+     * getter for cancelled transactions
+     * @return the cancelled transactions as a List
+     */
     public List<Transaction> getCancelledTransactions() {
         return cancelledTransactions;
     }
@@ -95,15 +159,28 @@ public class User extends Account {
         this.topTradingPartners.addAll(topTradingPartners);
     }
 
+    /**
+     * getter for if user is frozen,
+     * @return this user's frozen status( frozen or not frozen)
+     */
     public boolean getIsFrozen() {
         return isFrozen;
     }
 
+    /**
+     * setter for if user is frozen
+     * @param isFrozen to change the user's status of frozen or not frozen as boolean
+     */
     public void setIsFrozen(boolean isFrozen) {
         this.isFrozen = isFrozen;
 
     }
 
+    /**
+     * gettter for if user is PseudoFrzoen, PseuodoFrzoen means that the user's trade  is over the limit of weekly trades
+     * or incom
+     * @return
+     */
     public boolean getIsPseudoFrozen() {
         return isPseudoFrozen;
     }
