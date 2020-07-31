aa63626fb30905f35b0bb06e0e4fb87a2b98940d
Daniel Cordovi Caleyo <daniel.cordovicaleyo@mail.utoronto.ca>
2020-07-31 17:06:35 -0400

    Wrote javadoc for VacationPrompter and VacationUserMainMenu

--- phase2/src/VacationPrompter.java
+++ phase2/src/VacationPrompter.java
@@ -5,6 +5,23 @@ import java.util.logging.Logger;
 public class VacationPrompter implements userMainMenuOptions {
     Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Tells a User that their account status will be changed to "on vacation". If they confirm, this method removes
+     * all Items from this User's inventory. If they cancel, User is returned to the main menu.
+     * <p>
+     * Note: removed inventory Items are logged so they can be restored when this User returns from vacation.
+     *
+     * @param user             User currently logged into the system
+     * @param allItems         The instance of ItemManager
+     * @param allTradeRequests The instance of TradeRequestManager
+     * @param allUsers         The instance of UserManager
+     * @param allMeetings      The instance of MeetingManager
+     * @param allTransactions  The instance of TransactionManager
+     * @param allAdmins        The instance of AdminManager
+     * @param undoLogger       Logger that logs actions in the system
+     * @return null if the current menu is to be reprinted; User user if the user is to be redirected to the main menu;
+     * String "exit" if the user is to be logged out.
+     */
     public Object execute(User user, ItemManager allItems, TradeRequestManager allTradeRequests, UserManager allUsers,
                           MeetingManager allMeetings, TransactionManager allTransactions, AdminManager allAdmins,
                           Logger undoLogger) {
@@ -22,11 +39,9 @@ public class VacationPrompter implements userMainMenuOptions {
                         " from " + user.getName() + "'s inventory.\n");
             }
             return "exit";
-        }
-        else if (confirmation.equals("2")) {
+        } else if (confirmation.equals("2")) {
             return user;
-        }
-        else {
+        } else {
             System.out.println("Invalid input. Please try again.");
             return null;
         }
--- phase2/src/VacationUserMainMenu.java
+++ phase2/src/VacationUserMainMenu.java
@@ -3,9 +3,25 @@ import java.util.logging.Logger;
 
 public class VacationUserMainMenu implements DifferentUserMainMenu {
 
+    /**
+     * Displays the main menu for a User on vacation. Users on vacation can either stay on vacation or declare they have
+     * returned from vacation, in which case their inventory Items are restored and they can participate in the trade
+     * system again.
+     *
+     * @param user             User currently logged into the system
+     * @param allItems         The instance of ItemManager
+     * @param allTradeRequests The instance of TradeRequestManager
+     * @param allUsers         The instance of UserManager
+     * @param allMeetings      The instance of MeetingManager
+     * @param allTransactions  The instance of TransactionManager
+     * @param allAdmins        The instance of AdminManager
+     * @param undoLogger       Logger that logs actions in the system
+     * @return null if the current menu is to be reprinted; User user if the user is to be redirected to the main menu;
+     * String "exit" if the user is to be logged out.
+     */
     public Object mainMenu(User user, ItemManager allItems, TradeRequestManager allTradeRequests,
-                            UserManager allUsers, MeetingManager allMeetings, TransactionManager allTransactions,
-                            AdminManager allAdmins, Logger undoLogger) {
+                           UserManager allUsers, MeetingManager allMeetings, TransactionManager allTransactions,
+                           AdminManager allAdmins, Logger undoLogger) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("----------------------------------------------------------------------------------------------" +
                 "\n\uD83D\uDC4B Welcome back, " + user.getName() + "!\n");
@@ -14,16 +30,14 @@ public class VacationUserMainMenu implements DifferentUserMainMenu {
         if (confirmation.equals("1")) {
             System.out.println("Go enjoy your vacation!");
             return "exit";
-        }
-        else if (confirmation.equals("2")) {
+        } else if (confirmation.equals("2")) {
             System.out.println("We hope you enjoyed your vacation!");
             user.setIsOnVacation(false);
             //TODO: use undoLogger to add items back to user's inventory
             return user;
-        }
-        else {
+        } else {
             System.out.println("Invalid option. Please try again.");
-            return user;
+            return null;
         }
     }
 }
