addef0c8c0bbfb10adb02878c2bb9d3c8afdf8e9
tavalla2 <tina.tavallaeian@maill.utoronto.ca>
2020-08-11 21:26:57 -0400

    Finished adding to inventory.

--- phase2/src/system_menus/user_main_menus/options/InventoryManager.java
+++ phase2/src/system_menus/user_main_menus/options/InventoryManager.java
@@ -44,8 +44,7 @@ public class InventoryManager implements UserMainMenuOptions {
         }
         Scanner sc = new Scanner(System.in);
         //asks if the User wants to remove an item from their wishlist
-        System.out.println("If you would like to remove an item, please enter '1'. If you would like to change pricing, press '2'" +
-                "or type 'back'");
+        System.out.println("If you would like to remove an item, please enter '1' or type 'back' to go to main menu");
         Object input = sc.nextLine();
         //returns them to the main menu if they wish to go "back"
 
@@ -61,9 +60,12 @@ public class InventoryManager implements UserMainMenuOptions {
 
 
         if (input.equals("1")) {
-            System.out.print("Please enter the ID of the item you would like to change.\n");
+            System.out.print("Please enter the ID of the item you would like to change. Type 'back' to go back\n");
             Object input2 = sc.nextLine();
 
+            if (input2.equals("back")) {
+                return null;
+            }
 
             try {
                 //will try to turn the input into an integer
@@ -73,6 +75,7 @@ public class InventoryManager implements UserMainMenuOptions {
                 System.out.print("\u274CCommand Invalid. Please try again!\n");
                 return null;
             }
+
 
 
             if (((Integer) input2 < 1) || (Integer) input2 > in.size()) {
@@ -95,75 +98,11 @@ public class InventoryManager implements UserMainMenuOptions {
 
             System.out.println("\uD83D\uDDD1Item has been removed successfully!");
             return null;
-        } else if (input.equals("2")) {
+        }
 
-            System.out.print("Please enter the ID of the item you would like to change.\n");
-            Object input2 = sc.nextLine();
-
-
-            try {
-                //will try to turn the input into an integer
-                //if input is not an integer, returns null and recalls wishlist()
-                input2 = Integer.parseInt((String) input2);
-            } catch (NumberFormatException e) {
-                System.out.print("\u274CCommand Invalid. Please try again!\n");
-                return null;
-            }
-
-
-            if (((Integer) input2 < 1) || (Integer) input2 > in.size()) {
-                System.out.print("\uD83D\uDE35Input is out of bound. Please try again!\n");
-                return null;
-            }
-            //remove the item they requested from inventory
-            Item itemSelected = in.get((Integer) input2 - 1);
-
-            if (!itemSelected.getRentable() || !itemSelected.getSellable())
-            {
-                System.out.print("This item is not rentable or sellable!\n");
-                return user;
-            }
-
-            String output = "";
-            if (itemSelected.getRentable())
-                output = output + " [RENTABLE] at the price: $" + itemSelected.getRentPrice() + "\n";
-            else if (itemSelected.getSellable())
-                output = output + " [SELLABLE] at the price: $" + itemSelected.getSellPrice() + "\n";
-            else if (itemSelected.getTradable())
-                output = output + " [TRADABLE] ";
-
-            System.out.print("You have chosen the following item: " + itemSelected.getName() +
-                    "\nCurrently the item is:" + output + "\n");
-
-            //only sellable
-            if (itemSelected.getSellable() && !itemSelected.getRentable()){
-                System.out.print("Please enter the new price.\n");
-
-                 input2 = sc.nextLine();
-
-                try {
-                    //will try to turn the input into an integer
-                    //if input is not an integer, returns null and recalls wishlist()
-                    input2 = Integer.parseInt((String) input2);
-                } catch (NumberFormatException e) {
-                    System.out.print("\u274CCommand Invalid. Please try again!\n");
-                    return null;
-                }
-
-
-
-
-
-            }
-
-
-
-
-
-            return user;
-        } else {
+        else {
             System.out.print("\u274CCommand Invalid. Please try again!\n");
-            return user;
+            return null;
 
         }
     }
